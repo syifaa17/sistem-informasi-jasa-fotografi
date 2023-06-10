f035924ad28a94e97240fde51f214460068331af
syifaa17 <nurasyifa@students.polmed.ac.id>
2023-06-10 13:17:46 +0700

    feat : initial project

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+// import './bootstrap';
+
+import "../vendor/bootstrap/css/bootstrap.min.css"
+import "../vendor/bootstrap-icons/bootstrap-icons.css"
+import "../vendor/swiper/swiper-bundle.min.css"
+import "../vendor/glightbox/css/glightbox.min.css"
+import "../vendor/aos/aos.css"
+import "../css/main.css"
+
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+window.Alpine = Alpine;
+
+Alpine.plugin(focus);
+
+Alpine.start();
+
